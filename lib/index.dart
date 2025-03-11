938b194db19e5e516d9a10b4b65012d2cab02909
znanie63 <znanie63@yandex.ru>
2025-03-11 18:29:56 +0800

    Copy lib/index.dart from drivers-16.19.31

---
+++ lib/index.dart
@@ -0,0 +1,34 @@
+// Export pages
+export '/pages/auth_page/auth_page_widget.dart' show AuthPageWidget;
+export '/pages/driver/orders_page/orders_page_widget.dart'
+    show OrdersPageWidget;
+export '/pages/driver/trip_page/trip_page_widget.dart' show TripPageWidget;
+export '/pages/driver/statistics_page/statistics_page_widget.dart'
+    show StatisticsPageWidget;
+export '/pages/driver/choose_tariff_page/choose_tariff_page_widget.dart'
+    show ChooseTariffPageWidget;
+export '/pages/driver/finished_shift_page/finished_shift_page_widget.dart'
+    show FinishedShiftPageWidget;
+export '/pages/driver/finished_trip_page/finished_trip_page_widget.dart'
+    show FinishedTripPageWidget;
+export '/pages/admin/all_drivers_page/all_drivers_page_widget.dart'
+    show AllDriversPageWidget;
+export '/pages/admin/driver_page/driver_page_widget.dart' show DriverPageWidget;
+export '/pages/admin/info_page/info_page_widget.dart' show InfoPageWidget;
+export '/pages/admin/new_driver_page/new_driver_page_widget.dart'
+    show NewDriverPageWidget;
+export '/pages/admin/history_page/history_page_widget.dart'
+    show HistoryPageWidget;
+export '/pages/admin/driver_statistics_page/driver_statistics_page_widget.dart'
+    show DriverStatisticsPageWidget;
+export '/pages/admin/rates_page/rates_page_widget.dart' show RatesPageWidget;
+export '/pages/admin/rate_edit/rate_edit_widget.dart' show RateEditWidget;
+export '/pages/admin/admin_orders_page/admin_orders_page_widget.dart'
+    show AdminOrdersPageWidget;
+export '/pages/admin/new_order_page/new_order_page_widget.dart'
+    show NewOrderPageWidget;
+export '/nav_page/nav_page_widget.dart' show NavPageWidget;
+export '/pages/driver/payments/payments_widget.dart' show PaymentsWidget;
+export '/pages/admin/new_admin/new_admin_widget.dart' show NewAdminWidget;
+export '/pages/admin/info_page_admin/info_page_admin_widget.dart'
+    show InfoPageAdminWidget;
