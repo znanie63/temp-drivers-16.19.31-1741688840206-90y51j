3feb3c40c1bc0b0cd76cbdfc1368bc5d0df99de1
znanie63 <znanie63@yandex.ru>
2025-03-11 18:30:26 +0800

    Copy lib/pages/admin/new_admin/new_admin_model.dart from drivers-16.19.31

---
+++ lib/pages/admin/new_admin/new_admin_model.dart
@@ -0,0 +1,48 @@
+import '/backend/supabase/supabase.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/index.dart';
+import 'new_admin_widget.dart' show NewAdminWidget;
+import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+class NewAdminModel extends FlutterFlowModel<NewAdminWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode1;
+  TextEditingController? textController1;
+  String? Function(BuildContext, String?)? textController1Validator;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode2;
+  TextEditingController? textController2;
+  String? Function(BuildContext, String?)? textController2Validator;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode3;
+  TextEditingController? textController3;
+  final textFieldMask3 = MaskTextInputFormatter(mask: '+993 ## ######');
+  String? Function(BuildContext, String?)? textController3Validator;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode4;
+  TextEditingController? textController4;
+  String? Function(BuildContext, String?)? textController4Validator;
+  // Stores action output result for [Backend Call - Insert Row] action in Button widget.
+  UsersRow? createadmin;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    textFieldFocusNode1?.dispose();
+    textController1?.dispose();
+
+    textFieldFocusNode2?.dispose();
+    textController2?.dispose();
+
+    textFieldFocusNode3?.dispose();
+    textController3?.dispose();
+
+    textFieldFocusNode4?.dispose();
+    textController4?.dispose();
+  }
+}
