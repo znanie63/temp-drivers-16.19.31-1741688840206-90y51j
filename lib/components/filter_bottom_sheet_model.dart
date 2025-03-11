677ac0108e05f7dc7805378ccd2a94ace9e955cd
znanie63 <znanie63@yandex.ru>
2025-03-11 18:29:17 +0800

    Copy lib/components/filter_bottom_sheet_model.dart from drivers-16.19.31

---
+++ lib/components/filter_bottom_sheet_model.dart
@@ -0,0 +1,15 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'filter_bottom_sheet_widget.dart' show FilterBottomSheetWidget;
+import 'package:flutter/material.dart';
+
+class FilterBottomSheetModel extends FlutterFlowModel<FilterBottomSheetWidget> {
+  ///  Local state fields for this component.
+
+  String compStateFilter = 'all';
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
