24a7a9aa01ff2a6772db38afdb3ada75cd4df7f0
znanie63 <znanie63@yandex.ru>
2025-03-11 18:29:38 +0800

    Copy lib/flutter_flow/custom_icons.dart from drivers-16.19.31

---
+++ lib/flutter_flow/custom_icons.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+
+class FFIcons {
+  FFIcons._();
+
+  static const String _driversIconsFamily = 'DriversIcons';
+
+  // DriversIcons
+  static const IconData kadd =
+      IconData(0xe800, fontFamily: _driversIconsFamily);
+  static const IconData kback =
+      IconData(0xe801, fontFamily: _driversIconsFamily);
+  static const IconData kcopy =
+      IconData(0xe802, fontFamily: _driversIconsFamily);
+  static const IconData kclose =
+      IconData(0xe803, fontFamily: _driversIconsFamily);
+  static const IconData kday =
+      IconData(0xe805, fontFamily: _driversIconsFamily);
+  static const IconData kdelivery =
+      IconData(0xe806, fontFamily: _driversIconsFamily);
+  static const IconData kedit =
+      IconData(0xe808, fontFamily: _driversIconsFamily);
+  static const IconData kfilter =
+      IconData(0xe809, fontFamily: _driversIconsFamily);
+  static const IconData klogout =
+      IconData(0xe80b, fontFamily: _driversIconsFamily);
+  static const IconData kmakeRoute =
+      IconData(0xe80c, fontFamily: _driversIconsFamily);
+  static const IconData kmore =
+      IconData(0xe80d, fontFamily: _driversIconsFamily);
+  static const IconData knight =
+      IconData(0xe80e, fontFamily: _driversIconsFamily);
+  static const IconData kordersAdmin =
+      IconData(0xe80f, fontFamily: _driversIconsFamily);
+  static const IconData ktrip =
+      IconData(0xe810, fontFamily: _driversIconsFamily);
+  static const IconData ksober =
+      IconData(0xe811, fontFamily: _driversIconsFamily);
+  static const IconData kphone =
+      IconData(0xe812, fontFamily: _driversIconsFamily);
+  static const IconData kstatistics =
+      IconData(0xe813, fontFamily: _driversIconsFamily);
+  static const IconData kdrivers =
+      IconData(0xe814, fontFamily: _driversIconsFamily);
+  static const IconData kforward =
+      IconData(0xe815, fontFamily: _driversIconsFamily);
+  static const IconData ktariffs =
+      IconData(0xe816, fontFamily: _driversIconsFamily);
+}
