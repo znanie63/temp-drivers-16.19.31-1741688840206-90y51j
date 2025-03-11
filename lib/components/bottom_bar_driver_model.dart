f83753c045e059b3ca67baa4a3fe88f7517bf769
znanie63 <znanie63@yandex.ru>
2025-03-11 18:29:07 +0800

    Copy lib/components/bottom_bar_driver_model.dart from drivers-16.19.31

---
+++ lib/components/bottom_bar_driver_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'bottom_bar_driver_widget.dart' show BottomBarDriverWidget;
+import 'package:flutter/material.dart';
+
+class BottomBarDriverModel extends FlutterFlowModel<BottomBarDriverWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
