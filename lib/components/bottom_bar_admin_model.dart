a9b200aae75ea38c56e5a482a434ca7dd6cdd827
znanie63 <znanie63@yandex.ru>
2025-03-11 18:29:05 +0800

    Copy lib/components/bottom_bar_admin_model.dart from drivers-16.19.31

---
+++ lib/components/bottom_bar_admin_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'bottom_bar_admin_widget.dart' show BottomBarAdminWidget;
+import 'package:flutter/material.dart';
+
+class BottomBarAdminModel extends FlutterFlowModel<BottomBarAdminWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
