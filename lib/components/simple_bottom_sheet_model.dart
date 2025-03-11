003fecd7025d50dcb6efd55e65413cf14e322325
znanie63 <znanie63@yandex.ru>
2025-03-11 18:29:19 +0800

    Copy lib/components/simple_bottom_sheet_model.dart from drivers-16.19.31

---
+++ lib/components/simple_bottom_sheet_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'simple_bottom_sheet_widget.dart' show SimpleBottomSheetWidget;
+import 'package:flutter/material.dart';
+
+class SimpleBottomSheetModel extends FlutterFlowModel<SimpleBottomSheetWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
