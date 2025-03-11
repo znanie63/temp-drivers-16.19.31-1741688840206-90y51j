6c89f9dc1eb1b83ce691b942c7b08ac74c0f4f44
znanie63 <znanie63@yandex.ru>
2025-03-11 18:28:56 +0800

    Copy lib/backend/supabase/supabase.dart from drivers-16.19.31

---
+++ lib/backend/supabase/supabase.dart
@@ -0,0 +1,25 @@
+import 'package:supabase_flutter/supabase_flutter.dart' hide Provider;
+
+export 'database/database.dart';
+
+String _kSupabaseUrl = 'https://taxiapp.pro';
+String _kSupabaseAnonKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE';
+
+class SupaFlow {
+  SupaFlow._();
+
+  static SupaFlow? _instance;
+  static SupaFlow get instance => _instance ??= SupaFlow._();
+
+  final _supabase = Supabase.instance.client;
+  static SupabaseClient get client => instance._supabase;
+
+  static Future initialize() => Supabase.initialize(
+        url: _kSupabaseUrl,
+        anonKey: _kSupabaseAnonKey,
+        debug: false,
+        authOptions:
+            FlutterAuthClientOptions(authFlowType: AuthFlowType.implicit),
+      );
+}
