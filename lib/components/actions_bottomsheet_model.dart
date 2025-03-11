b125c883169dd0e3e9fbb7bebaa2acf2f74423c8
znanie63 <znanie63@yandex.ru>
2025-03-11 18:28:58 +0800

    Copy lib/components/actions_bottomsheet_model.dart from drivers-16.19.31

---
+++ lib/components/actions_bottomsheet_model.dart
@@ -0,0 +1,12 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'actions_bottomsheet_widget.dart' show ActionsBottomsheetWidget;
+import 'package:flutter/material.dart';
+
+class ActionsBottomsheetModel
+    extends FlutterFlowModel<ActionsBottomsheetWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
