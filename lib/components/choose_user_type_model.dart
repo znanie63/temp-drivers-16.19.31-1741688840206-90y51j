a9455ad2e85fa8ae0692ccc41af736db1d4b4623
znanie63 <znanie63@yandex.ru>
2025-03-11 18:29:11 +0800

    Copy lib/components/choose_user_type_model.dart from drivers-16.19.31

---
+++ lib/components/choose_user_type_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'choose_user_type_widget.dart' show ChooseUserTypeWidget;
+import 'package:flutter/material.dart';
+
+class ChooseUserTypeModel extends FlutterFlowModel<ChooseUserTypeWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
